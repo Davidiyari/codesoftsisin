6d527405ca5df7157a631ddbe0a19df34850c045
Davidiyari <garcia.david.2024@cbtis-246.edu.mx>
2025-05-10 16:17:17 -0600

    Add files via upload
    Aqui esta el codigo corregido una disculpa por la desorganización me esta costando usar github

--- archivos.py
+++ archivos.py
@@ -3,16 +3,16 @@ with open('alumnos.txt', 'r') as fichero:
     for linea in fichero:
         print(linea, end='')
 
-lista= ["Garcia Hernandez Sofia Gabriela\n",
-        "Sanchez Lopez Mateo Julian\n",
-        "Martinez Rodriguez Luciana Valeria\n",
-        "Gomez Gonzalez Alejandro Daniel\n",
-        "Torres Ramirez Valeria Sofia\n",
-        "Diaz Morales Sebastian Julian\n",
-        "Reyes Sanchez Gabriela Luciana\n",
-        "Hernandez Garcia Carlos Eduardo\n",
-        "Lopez Martinez Daniela Valeria\n",
-        "Ramirez Torres Julian Alejandro\n"]
+lista= ["Sanchez Ortiz Lizbeth Estefania\n",
+        "Bayardo Fregoso Carlos Eduardo \n",
+        "Gomez Fernandez Jose Roberto\n",
+        "Mejia Lopez Angel Gadiel\n",
+        "Parra Almado Emmanuel Alejandro\n",
+        "Montaño Hernandez Yamile Isabela\n",
+        "Robles Gomez Efren Alexander\n",
+        "Gutierrez Gomez Gerardo Jesus\n",
+        "Garcia Guzman Andrea Guadalupe\n",
+        "Romero Zaragoza Cristofer Alexander\n"]
 with open("datos_guardados.txt", 'w') as fichero:
     fichero.writelines(lista)
 
